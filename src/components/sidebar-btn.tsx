7a3e20ecedb4e82b770290a6a312be95b67fbf9c
jeelpatel231 <jeelpatel231@gmail.com>
2024-03-08 12:26:21 +0530

    show active path in sidebar button

--- src/components/sidebar-btn.tsx
+++ src/components/sidebar-btn.tsx
@@ -1,7 +1,24 @@
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
-export const SidebarBtn = ({ label, navigateTo }: { label:string, navigateTo: string }) => {
+export const SidebarBtn = ({
+  label,
+  navigateTo,
+  className,
+}: {
+  label: string;
+  navigateTo: string;
+  className?: string;
+}) => {
   return (
-    <Link to={navigateTo} className="rounded-full p-4 hover:bg-surface-variant focus:bg-secondary">{label}</Link>
+    <Link
+      to={navigateTo}
+      className={twMerge(
+        "rounded-full p-4 hover:bg-surface-variant focus:bg-secondary",
+        className
+      )}
+    >
+      {label}
+    </Link>
   );
-}
+};
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,42 +1,55 @@
+import { useLocation } from "react-router-dom";
 import { SidebarBtn } from "./sidebar-btn";
+import { twMerge } from "tailwind-merge";
 
 export const Sidebar = () => {
+  const location = useLocation();
+  const rootPath = "/" + location.pathname.split("/").filter((x) => x)[0];
+
   const sidebarBtns = [
     {
-      label: 'machine',
-      route: '/machine',
+      label: "machine",
+      route: "/machine",
     },
     {
-      label: 'machine imports',
-      route: '/machine-imports',
+      label: "machine imports",
+      route: "/machine-imports",
     },
     {
-      label: 'raw material',
-      route: '/raw-material',
+      label: "raw material",
+      route: "/raw-material",
     },
     {
-      label: 'raw material import',
-      route: '/raw-material-import',
+      label: "raw material import",
+      route: "/raw-material-import",
     },
     {
-      label: 'products',
-      route: '/products',
+      label: "products",
+      route: "/products",
     },
     {
-      label: 'product manufacturing',
-      route: '/product-manufacturing',
+      label: "product manufacturing",
+      route: "/product-manufacturing",
     },
     {
-      label: 'sales',
-      route: '/sales',
+      label: "sales",
+      route: "/sales",
     },
-  ].map(btnInfo => {
-    return (<SidebarBtn label={btnInfo.label} navigateTo={btnInfo.route}/>);
+  ].map((btnInfo) => {
+    return (
+      <SidebarBtn
+        label={btnInfo.label}
+        className={twMerge(rootPath === btnInfo.route && "bg-secondary")}
+        navigateTo={btnInfo.route}
+      />
+    );
   });
 
   return (
-  <div className="border-r-2 min-w-52 max-w-52 border-outline p-4 flex flex-col gap-1 h-full">
-    {sidebarBtns}
-  </div>
+    <div
+      className={`border-r-2 min-w-52 max-w-52 border-outline p-4 flex flex-col gap-1 h-full`}
+    >
+      {sidebarBtns}
+    </div>
   );
-}
+};
